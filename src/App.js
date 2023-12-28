bfbe0c517e4256c03a17ac370b01727046feb08e
김다경(Dakyeong Kim) <dkkim@feelanet.com>
2023-12-28 17:16:55 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Routes/home";
+import Detail from "./Routes/Detail";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
+
+//6.0 이상 버전
+//<Router>
+//<Routes>
+//<Route path="/" element={<Home />} />
+//<Route path="/movie" element={<Detail />} />
+//</Routes>
+//</Router>
